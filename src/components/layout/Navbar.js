2986f659d3c35111fdb7fd4ec81cbaa959570737
sharmese <qualsore@gmail.com>
2023-10-25 02:11:51 +0300

    fixed navigation buttons

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -4,18 +4,26 @@ const Navbar = () => {
   return (
     <nav className={classes.nav}>
       <ul>
-        <Link smooth={true} to='assortment'>
-          <li>АСОРТИМЕНТ</li>
-        </Link>
-        <Link smooth={true} to='about'>
-          <li>ПРО МАГАЗИН</li>
-        </Link>
-        <Link smooth={true} to='q&a'>
-          <li>Q&A</li>
-        </Link>
-        <Link smooth={true} to='reviews'>
-          <li>ВІДГУКИ</li>
-        </Link>
+        <li>
+          <Link smooth={true} to='assortment'>
+            АСОРТИМЕНТ
+          </Link>
+        </li>
+        <li>
+          <Link smooth={true} to='about'>
+            ПРО МАГАЗИН
+          </Link>
+        </li>
+        <li>
+          <Link smooth={true} to='reviews'>
+            ВІДГУКИ
+          </Link>
+        </li>
+        <li>
+          <Link smooth={true} to='q&a'>
+            Q&A
+          </Link>
+        </li>
       </ul>
     </nav>
   );
